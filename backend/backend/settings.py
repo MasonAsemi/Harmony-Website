a531eeeae263fed00d00d4bea4f04d6ad4b7f7c7
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-02 11:55:47 -0700

    setting up heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)jb@w%8f%4aa2+*tt-_an^8b-%lxs&$0x=26af92@&wzed&$3'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3)jb@w%8f%4aa2+*tt-_an^8b-%lxs&$0x=26af92@&wzed&$3')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG =True
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
