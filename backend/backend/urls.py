d7549ec8857b00d4551048912a6f2d5127ace03c
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-02 11:03:18 -0700

    Harmony App created
    This will be the route that the main web app interacts with

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("", include("harmony.urls")),
+    path("polls/", include("polls.urls")), #following the quickstart
     path('admin/', admin.site.urls),
 ]
