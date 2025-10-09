fabea8d7ceef31a7aa56ea5810ba33c21d09ad8d
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-08 22:45:39 -0700

    finished auto generated api docs

--- backend/harmony/serializers.py
+++ backend/harmony/serializers.py
@@ -6,3 +6,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = User
         fields = ['id', 'username', 'password', 'email']
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True, help_text="Username for the account")
+    email = serializers.EmailField(required=True, help_text="User's email address")
+    password = serializers.CharField(required=True, write_only=True, help_text="Password (min 8 characters)")
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True, help_text="Your username")
+    password = serializers.CharField(required=True, write_only=True, help_text="Your password")
+
+class TokenResponseSerializer(serializers.Serializer):
+    token = serializers.CharField(help_text="Authentication token")
+    user = serializers.JSONField(help_text="User information")
--- backend/harmony/views.py
+++ backend/harmony/views.py
@@ -1,14 +1,28 @@
 #run code for any api whenever it gets called 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response # generate json responses 
-
-from .serializers import UserSerializer
+from .serializers import UserSerializer , RegisterSerializer, LoginSerializer, TokenResponseSerializer
 from rest_framework import status 
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
-
 from django.shortcuts import get_object_or_404
+#We want to be able to pass in an authtoken and get a respective user 
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+#LOGIN
+@swagger_auto_schema(
+    method='post',
+    request_body=LoginSerializer,
+    responses={
+        200: TokenResponseSerializer,
+        404: 'Unauthorized - user not found'
+    },
+    operation_description="Login with username and password to receive authentication token"
+)
 @api_view(['POST'])
 def login(request):
 
@@ -22,6 +36,16 @@ def login(request):
     
     return Response({"token": token.key, "user":serializer.data})   
 
+#REGISTER
+@swagger_auto_schema(
+    method='post',
+    request_body=RegisterSerializer,
+    responses={
+        201: TokenResponseSerializer,
+        400: 'Bad Request - validation errors'
+    },
+    operation_description="Register a new user account"
+)
 @api_view(['POST'])
 def register(request):
     
@@ -39,16 +63,35 @@ def register(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
 
-
-#We want to be able to pass in an authtoken and get a respective user 
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-
+#TEST TOKEN
+@swagger_auto_schema(
+    method='get',
+    manual_parameters=[
+        openapi.Parameter(
+            'Authorization',
+            openapi.IN_HEADER,
+            description="Token <your_token_here>",
+            type=openapi.TYPE_STRING,
+            required=True
+        )
+    ],
+    responses={
+        200: openapi.Response(
+            description="Token is valid",
+            examples={
+                "application/json": {
+                    "message": "Token is valid",
+                    "user": "username"
+                }
+            }
+        ),
+        401: 'Unauthorized - invalid or missing token'
+    },
+    operation_description="Test if your authentication token is valid"
+)
 # api only works if user is authenticated 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, TokenAuthentication])
 @permission_classes([IsAuthenticated]) 
 def test_token(request):
-    return Response("passed for user: {}".format(request.user.email)) #request should have the user if this func is envoked
+    return Response({"message": "Token is valid","user": request.user.username}) #request should have the user if this func is envoked
