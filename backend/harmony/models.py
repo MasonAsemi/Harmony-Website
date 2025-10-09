49a7aa886f4a1bbd79e329c04587f6e9ac6b2f13
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-08 21:08:28 -0700

    Update models.py

--- backend/harmony/models.py
+++ backend/harmony/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200) #stored encrypted 
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
@@ -9,17 +10,3 @@ class User(models.Model):
     
     class Meta:
         ordering = ['-created_at']  # Most recent first
-
-class TeamMember(models.Model):
-    name = models.CharField(max_length=100)
-    role = models.CharField(max_length=100)
-    experience = models.CharField(max_length=50)
-    description = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    class Meta:
-        db_table = 'team_members'
-        ordering = ['id']
-    
-    def __str__(self):
-        return self.name
