3e1ca30460269652cd8df85b69b44522d509a071
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-08 22:54:26 -0700

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-3)jb@w%8f%4aa2+*tt-_a
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
