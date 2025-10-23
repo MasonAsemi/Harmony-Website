d3158580613c69b6cc7d7cb2fc94c6ad29a8bd1e
MasonAsemi <149756149+MasonAsemi@users.noreply.github.com>
2025-10-23 00:44:32 -0700

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // Always use the environment variable or fall back to production backend
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://harmony-backend-4080-0c4993847d89.herokuapp.com/api';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://harmony-backend-4800-3fc8f73b6cb5.herokuapp.com/api/';
 
 console.log('API_BASE_URL:', API_BASE_URL);
