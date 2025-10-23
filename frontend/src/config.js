a3dad2194f2e24cde2da0f86497b484d0fc81760
MasonAsemi <149756149+MasonAsemi@users.noreply.github.com>
2025-10-23 01:16:26 -0700

    Config Fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // Remove the trailing '/api/' - we'll add it in the API calls
-export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://harmony-backend-4800-3fc8f73b6cb5.herokuapp.com';
+export const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 console.log('API_BASE_URL:', API_BASE_URL);
