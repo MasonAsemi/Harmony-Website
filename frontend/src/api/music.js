236d7b91de7db19942ba647c299e091376c30e23
Jonathan Woline <jonwoline@gmail.com>
2025-10-24 01:24:25 -0700

    Song search and adding songs to profile

---
+++ frontend/src/api/music.js
@@ -0,0 +1,51 @@
+import { API_BASE_URL } from "../config";
+import axios from 'axios'
+
+/**
+ * API call through the backend to Spotify for querying a song 
+ * @param {string} query - The query to send for the search 
+ * @returns Promise for the search request.
+ */
+export const searchSong = (searchQuery) => {
+    // Fetch with the authorization token
+    return axios.get(`${API_BASE_URL}search/`, {
+        params: {
+            query: searchQuery
+        },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${localStorage.getItem("token")}`
+        }
+    })
+};
+
+export const addUserSong = (song) => {
+    // Fetch with the authorization token
+    return axios.post(`${API_BASE_URL}songs/`, song, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${localStorage.getItem("token")}`
+        }
+    })
+};
+
+export const deleteUserSong = (song) => {
+    // Fetch with the authorization token
+    return axios.delete(`${API_BASE_URL}songs/`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${localStorage.getItem("token")}`
+        }, 
+        data: song
+    })
+};
+
+export const getUserSongs = () => {
+    // Fetch with the authorization token
+    return axios.get(`${API_BASE_URL}songs`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${localStorage.getItem("token")}`
+        }
+    })
+};
--- frontend/src/components/SongSearch.jsx
+++ frontend/src/components/SongSearch.jsx
@@ -1,35 +1,105 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 import harmonyLogo from "../assets/logo.png";
+import { addUserSong, getUserSongs, searchSong } from "../api/music";
+import SongList from "./SongList";
 
 const SongSearch = () => {
     const [input, setInput] = useState('');
     const [songs, setSongs] = useState([]);
+    const [selectedSongs, setSelectedSongs] = useState([]);
+    const [cooldown, setCooldown] = useState(false);
+    const updateDelayMS = 500;
+    const autosearchThreshold = 1;
+    const enableAutosearch = true;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(input);
+    useEffect(() => {
+        getUserSongs().then((res) => {
+            setSelectedSongs(res.data)
+        })
+    }, [])
+
+    const handleOnChange = (e) => {
+        const currInput = e.target.value;
+        setInput(currInput)
+        
+        if (!enableAutosearch)
+            return;
+
+        if (!cooldown && currInput.length > autosearchThreshold)
+        {
+            setCooldown(true)
+            setTimeout(() => {
+                setCooldown(false)
+                if (e.target.value.length <= autosearchThreshold)
+                {
+                    setSongs([])
+                }
+                else
+                {
+                    sendQuery(e.target.value)
+                }
+            }, updateDelayMS)
+        }
+        else if (currInput.length <= autosearchThreshold)
+        {
+            setSongs([])
+        }
+    }
+
+    const sendQuery = async (query) => {
+        try {
+            const res = await searchSong(query)
+            if (res.status == 200)
+            {
+                setSongs(res.data.songs);
+            }
+            else
+            {
+                throw new Error("Song search failed");
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e?.preventDefault();
+        const searchQuery = input;
+        sendQuery(searchQuery);
+    }
+
+    const handleSongSelect = (index) => {
+        let updatedSongs = [...songs];
+        const selectedSong = updatedSongs.splice(index, 1)[0]
+        setSongs(updatedSongs);
+        addUserSong(selectedSong)
+
+        let updatedSelectedSongs = [...selectedSongs];
+        updatedSelectedSongs.push(selectedSong);
+        setSelectedSongs(updatedSelectedSongs)
     }
 
     return (
-    <div className="content-field">
+    <div className="content-field flex flex-col gap-3">
         <div className="field-title">Search for your favorite songs</div>
         <form onSubmit={handleSubmit} className="flex flex-row">
             <input
                 type="text"
                 value={input}
-                onChange={(e) => setInput(e.target.value)}
+                onChange={handleOnChange}
                 placeholder="Search..."
                 required
                 className="border"
             />
         </form>
-        {songs.length < 1 ? null : <div className="flex flex-col gap-2 border shadow-md p-4 overflow-y-scroll max-h-48">
+        {songs.length < 1 ? null : <div className="flex flex-col gap-2 border shadow-md p-4 overflow-y-auto max-h-48">
             {songs.map((item, index) => {
-                return (<div key={index}>{item.song_name}</div>)
+                return (<button onClick={() => {handleSongSelect(index)}} className="hover:bg-[#ddd] p-1 text-left" key={index}>{`${item.name} by ${item.list_of_artists[0].name}`}</button>)
             })}
         </div>}
+        <SongList songs={selectedSongs} setSongs={setSelectedSongs}/>
     </div>
     );
 }
