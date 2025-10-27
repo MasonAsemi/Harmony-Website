85031832a9a6019c163bf610c6bdb43ccdfdc152
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-10-26 17:37:55 -0700

    testing redirects with spotify api

--- backend/harmony/urls.py
+++ backend/harmony/urls.py
@@ -1,7 +1,7 @@
 # harmony/urls.py
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import UserViewSet, song_search, SongViewSet
+from .views import UserViewSet, song_search, SongViewSet, spotify_callback, spotify_login
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
@@ -10,5 +10,7 @@ router.register(r'songs', SongViewSet, basename='song')
 urlpatterns = [
     path('', include(router.urls)),
     path('search/', song_search),
+    path('spotify-auth/login/', spotify_login),
+    path('spotify-auth/callback/', spotify_callback),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # to obtain token
 ]
