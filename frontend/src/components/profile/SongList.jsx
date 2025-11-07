98b1dc1b5e6a940962cd81e3300c35afa7a06cb7
Kelechi Duru <75586620+KELECHIDAVIS@users.noreply.github.com>
2025-11-06 20:02:38 -0800

    Profile reload limit in place

--- frontend/src/components/profile/SongList.jsx
+++ frontend/src/components/profile/SongList.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { deleteUserSong } from "../../api/music";
 
-const SongList = ({ songs, setSongs }) => {
+const SongList = ({ songs, setSongs , numSongs}) => {
+    const [showList , setShowList]    = useState(false); // to make sure we're not calling the spotify embed api too much  
     const handleClick = async (index) => {
         let updatedSongs = [...songs];
         const removedSong = updatedSongs.splice(index, 1)
@@ -20,12 +21,18 @@ const SongList = ({ songs, setSongs }) => {
         }
     }
 
+    useEffect(() =>{
+        console.log("Showing songs")
+        setShowList(true); 
+    }, [])
     return (
     <div>
-        <div className="field-title">Favorite songs</div>
+        <div className="field-title">Favorite song(s):</div>
         {songs.length < 1 ? null : <ul className="flex flex-col gap-2 border shadow-md p-4 overflow-y-auto max-h-48">
             {songs.map((song, index) => {
-                if(song && song.embed && song.embed.html){
+
+                const limit = numSongs > 0 ? numSongs : 1  
+                if(song && song.embed && song.embed.html && showList && index < limit ){
                     return (
                         <li key={index} dangerouslySetInnerHTML={{ __html: song.embed.html }} />
                     )
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,6 @@
 // Trailing API is removed, but still appears?
-export const API_BASE_URL = 'https://harmony-backend-4800-3fc8f73b6cb5.herokuapp.com';
+const loaded_url = import.meta.env.VITE_API_URL;
+console.log("loaded_url: "+  loaded_url) 
+export const API_BASE_URL =  loaded_url || 'https://harmony-backend-4800-3fc8f73b6cb5.herokuapp.com';
 
 console.log('API_BASE_URL:', API_BASE_URL);
