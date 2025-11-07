904391d781fa07bcf570fdc81914d634f206ceea
Daniel Castillo <dcastillo2@cpp.edu>
2025-11-06 21:41:38 -0800

    feat(channels): Fix ASGI routing, WebSockets connection works, daphne works.

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -1,7 +1,6 @@
-from django.urls import path, re_path
+from django.urls import re_path
 from .consumers import Consumer
 
-# /api/chat/connect Connect to the websocket    
 websocket_urlpatterns = [
-    re_path(r"^ws/chat/connect/?$", Consumer.as_asgi())
+    re_path(r"ws/chat/connect/?", Consumer.as_asgi()),
 ]
--- frontend/src/api/chat.js
+++ frontend/src/api/chat.js
@@ -1,9 +1,9 @@
 import { WS_BASE_URL } from "../config";
 
 export const connectWebsocket = () => {
-    try {
-        return new WebSocket(`ws://localhost:8000/ws/chat/connect/`);
-    } catch (error) {
-        console.error(error);
-    }
-}
+  try {
+    return new WebSocket(`${WS_BASE_URL}/ws/chat/connect/`);
+  } catch (error) {
+    console.error("WebSocket connection failed:", error);
+  }
+};
